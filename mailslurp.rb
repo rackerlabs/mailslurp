39fc81dab57a5f337bdb82d8125241ffce09e17e
Ash Wilson <smashwilson@gmail.com>
2014-05-09 11:23:59 -0400

    Grab the queue from fog.

--- mailslurp.rb
+++ mailslurp.rb
@@ -1,10 +1,36 @@
 #!/bin/env ruby
 
 require 'sinatra/base'
+require 'fog'
+require 'yaml'
 
 class Mailslurp < Sinatra::Base
 
+  configure do
+    enable :logging
+
+    path = File.join(__dir__, 'mailslurp.yml')
+    @mailconf = YAML.load_file(path)
+
+    # Log in to Cloud Queues.
+    service = Fog::Rackspace::Queues.new(
+      :provider => 'rackspace'
+      :rackspace_username => @mailconf['rackspace_username'],
+      :rackspace_api_key => @mailconf['rackspace_api_key'],
+      :rackspace_region => @mailconf['rackspace_region']
+    )
+
+    @q = service.queues.get(:key => @mailconf['queue_name'])
+    if @q.nil?
+      @q = service.queues.create(:key => @mailconf['queue_name'])
+    end
+  end
+
   post '/incoming' do
+    logger.info "Got a message!"
+    logger.info params.inspect
+
+    200
   end
 
 end
