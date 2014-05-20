1b391f2e8dc1385c4af14b56b569aeebf5eb9a77
Ash Wilson <smashwilson@gmail.com>
2014-05-20 10:54:25 -0400

    Stash the queue in a class variable.

--- mailslurp.rb
+++ mailslurp.rb
@@ -5,25 +5,36 @@ require 'fog'
 require 'yaml'
 require 'time'
 
+class TheQueue
+  def self.login
+    path = File.join(__dir__, 'mailslurp.yml')
+    mailconf = YAML.load_file(path)
+
+    # Log in to Cloud Queues.
+    service = Fog::Rackspace::Queues.new(
+      :rackspace_username => mailconf['rackspace_username'],
+      :rackspace_api_key => mailconf['rackspace_api_key'],
+      :rackspace_region => mailconf['rackspace_region']
+    )
+
+    # Find or create the queue.
+    @q = service.queues.get(mailconf['queue_name'])
+    if @q.nil?
+      @q = service.queues.create(:name => mailconf['queue_name'])
+    end
+  end
+
+  def self.enqueue data
+    @q.enqueue data, 3600
+  end
+end
+
 class Mailslurp < Sinatra::Base
 
   configure do
     enable :logging
 
-    path = File.join(__dir__, 'mailslurp.yml')
-    @mailconf = YAML.load_file(path)
-
-    # Log in to Cloud Queues.
-    service = Fog::Rackspace::Queues.new(
-      :rackspace_username => @mailconf['rackspace_username'],
-      :rackspace_api_key => @mailconf['rackspace_api_key'],
-      :rackspace_region => @mailconf['rackspace_region']
-    )
-
-    @q = service.queues.get(@mailconf['queue_name'])
-    if @q.nil?
-      @q = service.queues.create(:name => @mailconf['queue_name'])
-    end
+    TheQueue.login
   end
 
   get '/' do
@@ -37,13 +48,11 @@ class Mailslurp < Sinatra::Base
     # Construct an event body from the message.
     subject = params['Subject'].gsub(/^(?:(?:Re:|Fwd:)\s*)+/, '')
 
-    event = {
+    TheQueue.enqueue(
       reporter: 'mailslurp',
       title: subject,
       incident_date: Time.parse(params['Date']).to_i
-    }
-
-    @q.enqueue event, 3600
+    )
 
     200
   end
